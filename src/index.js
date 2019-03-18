01341fa4606c28de39eafc558548c0aa8c562edb
Alena-F <alena_fetisova@mail.ru>
2019-03-18 21:26:07 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  if (str.length%2 !== 0) {
+    return false
+  }
+
+  let opens = [];
+  let closers = [];
+  for( let j=0; j<bracketsConfig.length; j++){
+    opens.push(bracketsConfig[j][0]);
+    closers.push(bracketsConfig[j][1])
+  }
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if(opens.indexOf(str[i]) !== -1) { 
+      if (opens.indexOf(str[i])===closers.indexOf(str[i]) && str[i] === stack[stack.length-1]) {
+        stack.pop(); 
+      } else {
+        stack.push(str[i]);
+      }
+    } 
+    else if (closers.indexOf(str[i]) !== -1 && closers.indexOf(str[i]) === opens.indexOf(stack[stack.length-1])) {
+        stack.pop();
+      } 
+      else {
+        return false;
+      }
+    }
+
+  if (stack.length > 0) return false;
+   
+  return true
 }
+
